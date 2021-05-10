720e65a6616fc18005106a42f29953d6b41640ec
Awesome221 <82426883+Awesome221@users.noreply.github.com>
2021-05-10 13:12:52 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,9 @@
+var slt = document.getElementById('lang');
+var lng = slt.selectedIndex;
+var lgp = slt.options[lng].value;
+var lg = slt.options[lng].innerHTML;
 
-	var slt = document.getElementById('lang');
-    var lng = slt.selectedIndex;
-    var lgp = slt.options[lng].value;
-    var lg = slt.options[lng].innerHTML;
-
-function English(){
+function Langues() {
 	var xhttp= new XMLHttpRequest();
 	xhttp.onreadystatechange = function(){
 		if(this.readyState == 4 && this.status == 200){
@@ -25,6 +24,7 @@ function English(){
 		xhttp.send();
 		}
 }
+
 var cpt, sld;
 
 var flag = -1;
@@ -49,24 +49,12 @@ var key_vrf = /^[0-9]{4}$/;
                 var code = document.getElementById('code').value;
 
 	if(code == tabCodes[id]){
-		if(lg == "En" || lg == "Fr")
-		{
-			key_m.textContent = 'ok';
-		}if(lg == "Wo"){
-			key_m.textContent = 'bakhna';
-		}
-			key_m.style.color = 'green';
-			mef.style.display = 'block';
-		
+		key_m.textContent = 'ok';
+		key_m.style.color = 'green';
+		mef.style.display = 'block';
 	}
 	else{
-		if(lg == "Fr"){
-			key_m.textContent = 'Ce code ne correspond pas à votre numéro de téléphone!';
-		}if(lg == "Wo"){
-			key_m.textContent = 'Code bi gua bouggal méggo wout ak sa numero!';
-		}if(lg == "En"){
-			key_m.textContent = 'this code doesn\'t match your phone number!'; 
-		}
+		key_m.textContent = 'Ce code ne correspond pas à votre numéro de téléphone!';
 		key_m.style.color = 'brown';
 		mef.style.display = 'none';
 	}
@@ -78,12 +66,7 @@ var mef = document.getElementById('bton');
 function afficherSolde(){
 	var select = document.getElementById('num');
     var idx = select.selectedIndex;
-    if(lg == "Fr"){
-	alert('Le solde de votre compte est de '+tabComptes[idx]);}
-    if(lg == "En"){
-	alert('Your account balance is '+tabComptes[idx]);}
-    if(lg == "Wo"){
-	alert(tabComptes[idx]+' mo nekka si sa kalpé Sen Money...');}
+	alert('Le solde de votre compte est de '+tabComptes[idx]);
 }
 
 function mobilMoney(){
@@ -97,38 +80,17 @@ var id = tabNumeros.indexOf(val);
             var code = document.getElementById('code').value;
 
 	if(code == tabCodes[id]){
-		if(lg == "Fr"){
- 	var chx = prompt("----Menu SENMONEY---\nTaper le numéro du service choisi\n1. Solde de mon compte\n2. Transfert dargent\n3. Paiement de facture\n4. Paramètre");
- 			}
- 		if(lg == "En"){
- 	var chx = prompt("----Menu SENMONEY---\nEnter your choose service number\n1. Account\n2. Money Transfer\n3. Bill payment\n4. Setting");
- 			}
- 		if(lg == "Wo"){
- 	var chx = prompt("----Menu SENMONEY---\nBindal fi numéro bi gua yitéwo\n1. Kholal sa guirsa\n2. Yooné xaliss\n3. Fayal sa facture\n4. taneff");
- 			}
+	
+ 	var chx = prompt("----Menu SENMONEY---\nTaper le numéro du service choisi\n1. Solde de mon compte\n2. Transfert dargent\n3. Paiement de facture\n4. Options");
 
  	if(chx == 1){
  		afficherSolde();
  	}
  	if(chx == 2){
- 		if(lg == 'Fr'){
-	 		var env = prompt("Saisissez le numéro du destinataire\n");
-	 		if(!isNaN(env)){
-	 			var senv = prompt("Le montant à envoyé");
-	 		}
-	 	}
-	 	if(lg == 'En'){
-	 		var env = prompt("Enter the recipient's number\n");
-	 		if(!isNaN(env)){
-	 			var senv = prompt("How much do you want to send it?");
-	 		}
-	 	}
-	 	if(lg == 'Wo'){
-	 		var env = prompt("Bindal fi Numéro bi kho wara diott\n");
-	 		if(!isNaN(env)){
-	 			var senv = prompt("Gnata gua koy yooné?");
-	 		}
-	 	}
+ 		var env = prompt("Saisissez le numéro du destinataire\n");
+ 		if(!isNaN(env)){
+ 			var senv = prompt("Le montant à envoyé");
+ 		}
 
 	    		var idx = select.selectedIndex;
 
@@ -145,26 +107,12 @@ var id = tabNumeros.indexOf(val);
 	    		 if(flag != -1){
 	    		 		sld = tabComptes[flag] + senV;
  						tabComptes[flag] = sld;
-if(lg == "Fr"){
+
 	    		alert('Votre transfert a bien été envoyé.\nA Bientôt, cher(e) client!');
-    		}
-if(lg == "En"){
-	    		alert('Your transfer has been sent successfully.\nSee you soon, dear customer!');
-    		}
-if(lg == "Wo"){
-	    		alert('Sa yobal dem na thie lo wor.\nFi akh thie kanam, dieureudieuf thie koloutébi!');
-    		}
+    		
 	    		 	}
 	    		 	else{
-if(lg == "Fr"){
 	    		 		alert('Votre numéro n\'est pas pris en compte!\nA Bientôt...');
-    		}
-if(lg == "En"){
-	    		 		alert('Your number is not taken into account!\nSee you soon...');
-    		}
-if(lg == "Wo"){
-	    		 		alert('Nio gui diégalu, wayé xamé wouniu sa numéro bi!\nBa bénen su niou xarit...');
-    		}
 	    		 	}
 	    		 
     		}
@@ -176,48 +124,31 @@ if(lg == "Wo"){
  			}
  }
  else{
- 	if(lg == "Fr"){
- 	alert('Votre compte est insuffisant pour cette opération!\nA Bientôt ... ');}
- 	 	if(lg == "En"){
- 	alert('Your account is not enough for this operation!\nSee you soon ... ');}
- 	 	if(lg == "Wo"){
- 	alert('Li gua beugg yooné néwoul thie sa guirsa!\nBa bénen yoon ... ');}
+ 	alert('Votre compte est insuffisant pour cette opération!\nA Bientôt ... ');
  }
  	}
  	if(chx == 3){
- 		if(lg == "Fr"){
- 		var pf = prompt("---Paiement de facture---\nChoix du service\n1.Senelec\n2.Sen'Eau\n3.Sonatel");}
- 		if(lg == "En"){
- 		var pf = prompt("---Bill Payment---\nService choose\n1.Senelec\n2.Sen'Eau\n3.Sonatel");}
- 		if(lg == "Wo"){
- 		var pf = prompt("---Fayal sa facture---\nTanal dieumoukay bi\n1.Senelec\n2.Sen'Eau\n3.Sonatel");}
+ 		var pf = prompt("---Paiement de facture---\nChoix du service\n1.Senelec\n2.Sen'Eau\n3.Sonatel");
 
- 		if(pf == 1 || pf == 2){
- 			if(lg == "Fr"){
- 			var cpt = prompt("Renseigner votre numéro de compteur");}
- 			if(lg == "En"){
- 			var cpt = prompt("Enter your counter number");}
- 			if(lg == "Wo"){
- 			var cpt = prompt("Dougalal sa numéro compteur");}
- 			if(!isNaN(cpt)){
- 				if(lg == "Fr"){	
- 				var pay = prompt("Votre montant à payer");}
- 				if(lg == "En"){	
- 				var pay = prompt("Your amount to pay");}
- 				if(lg == "Wo"){	
- 				var pay = prompt("Li gua wara faye");}
+ 		if(pf == 1){
+ 			var cpt = prompt("Renseigner votre numéro de compteur");
+ 			if(!isNaN(cpt)){	
+ 				var pay = prompt("Votre montant à payer");
  			}
 	 		if(!isNaN(pay)){
-	 			if(lg == "Fr"){
-	 			alert('Votre paiement a bien été enrégistré.\nAu revoir, cher(e) client!');}
-	 			if(lg == "En"){
-	 			alert('Your payment has been recorded.\nThank you, soon ...');}
-	 			if(lg == "Wo"){
-	 			alert('Sa fayoum facture bi dialana.\nDieureudieuf thie koloutébi!');}
+	 			alert('Votre paiement a bien été enrégistré.\nAu revoir, cher(e) client!');
+	 		}
+ 		}
+ 		if(pf == 2){
+ 			var cpt = prompt("Renseigner votre numéro de compteur");
+ 			if(!isNaN(cpt)){	
+ 				var pay = prompt("Votre montant à payer");
+ 			}
+	 		if(!isNaN(pay)){
+	 			alert('Votre paiement a bien été enrégistré.\nAu revoir, cher(e) client!');
 	 		}
  		}
  		if(pf == 3){
- 			if(lg == "Fr"){
  			var cpt = prompt("Renseigner votre numéro de téléphone");
  			if(!isNaN(cpt)){	
  				var pay = prompt("Votre montant à payer");
@@ -225,54 +156,20 @@ if(lg == "Wo"){
 	 		if(!isNaN(pay)){
 	 			alert('Votre paiement a bien été enrégistré.\nAu revoir, cher(e) client!');
 	 		}
-	 			}
- 			if(lg == "En"){
- 			var cpt = prompt("Enter your phone number");
- 			if(!isNaN(cpt)){	
- 				var pay = prompt("Your amount to pay");
- 			}
-	 		if(!isNaN(pay)){
-	 			alert('Your payment has been recorded.\nThank you, soon ...');
-	 		}
-	 			}
- 			if(lg == "Wo"){
- 			var cpt = prompt("Dougalal sa numéro téléphone");
- 			if(!isNaN(cpt)){	
- 				var pay = prompt("Li gua wara faye");
- 			}
-	 		if(!isNaN(pay)){
-	 			alert('Sa fayoum facture bi dialana.\nDieureudieuf thie koloutébi!');
-	 		}
-	 			}
  		}
  	}
  	if(chx == 4){
- 		var opt = prompt("---Options---\n1.Changer votre code\n2.Revenir à l'acceuil");
+ 		var opt = prompt("---Options---\n1.Paramètre\n2.Revenir à l'acceuil");
 
  		if(opt == 1){
- 			if(lg == "Fr"){
- 			alert('Changement du code\nA Bientôt...');}
- 			if(lg == "En"){
- 			alert('Changing your code\nComing soon...');}
- 			if(lg == "Wo"){
- 			alert('Bessal sa code\nLégui té gua meuna bessalat sa code...');}
+ 			alert('Paramètrage de votre compte\nA Bientôt...');
  		}
  		if(opt == 2){
- 			if(lg == "Fr"){
- 			alert('Au revoir!');}
- 			if(lg == "En"){
- 			alert('Goodbye');}
- 			if(lg == "Wo"){
- 			alert('Au revoir!');}
+ 			alert('Au revoir!');
  		}
  	}
  }
  else{
- 	if(lg == "Fr"){
- 	alert('Vous devez au préalable entré votre code!');}
- 	if(lg == "En"){
- 	alert('You must first enter your code!');}
- 	if(lg == "Wo"){
- 	alert('Faw gua Dougal sa code! bala guay meuna diala');}
+ 	alert('Vous devez au préalable entré votre code!');
  }
 }
